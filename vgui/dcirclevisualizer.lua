e34949e21c283498dfb7d746d5a1b9c8cd24ac5f
Patrick Emery <patrick.emery@outlook.com>
2019-06-21 23:23:10 -0400

    Update dcirclevisualizer.lua

--- vgui/dcirclevisualizer.lua
+++ vgui/dcirclevisualizer.lua
@@ -1,6 +1,6 @@
 --[[
-    DCircleVisualizer Panel.
-    A basic symmetric circular visualizer.
+    DVisualizer Panel.
+    A basic symmetric line visualizer.
     Usage:
         Create a panel with vgui.Create("DVisualizer"), then set
         the IGModAudioChannel with panel:SetSong( channel ).
@@ -15,4 +15,4 @@ function visualizer:SetRotation( x )
     self.rot = x
 end
 -- The functions below do not require documentation.
-local function a(b,c,d,e)return d+b*math.cos(c),e+b*math.sin(c)end;local f=0;local function g(h,c)if c then return h*math.pi+f else return math.pi+(1-h)*math.pi+f end end;function visualizer:Paint(i,j)if IsValid(self.song)then f=f+(self.rot or 0);self.song:FFT(self.bin,FFT_256)if#self.bin>=1 then local k=#self.bin;local l=math.min(i,j)/3;local m,n=a(l+l*self.bin[1],g(1/k,true),i/2,j/2)local d=0;local e=0;draw.NoTexture()surface.SetDrawColor(self.color)for o=1,k do d,e=a(l+l*self.bin[o%k+1],g(o/k,true),i/2,j/2)surface.DrawLine(m,n,d,e)m=d;n=e end;for o=k,1,-1 do d,e=a(l+l*self.bin[o%k+1],g(o/k,false),i/2,j/2)surface.DrawLine(m,n,d,e)m=d;n=e end;local m,n=a(l+l*self.bin[1],g(1/k,true),i/2,j/2)surface.DrawLine(m,n,d,e)else end end end;vgui.Register("DCircleVisualizer",visualizer,"DVisualizer")
+local function a(b,c,d,e)return d+b*math.cos(c),e+b*math.sin(c)end;local function f(g,c)if c then return g*math.pi else return math.pi+(1-g)*math.pi end end;function visualizer:Paint(h,i)surface.SetDrawColor(self.color)if IsValid(self.song)then self.r=(self.r or 0)+(self.rot or 0)self.song:FFT(self.bin,FFT_256)if#self.bin>=1 then local j=#self.bin;local k=math.min(h,i)/3;local l,m=a(k+k*self.bin[1],f(1/j,true),h/2,i/2)local d=0;local e=0;draw.NoTexture()for n=1,j do d,e=a(k+k*self.bin[n%j+1],f(n/j,true),h/2,i/2)surface.DrawLine(l,m,d,e)l=d;m=e end;for n=j,1,-1 do d,e=a(k+k*self.bin[n%j+1],f(n/j,false),h/2,i/2)surface.DrawLine(l,m,d,e)l=d;m=e end;local l,m=a(k+k*self.bin[1],f(1/j,true),h/2,i/2)surface.DrawLine(l,m,d,e)else local k=math.min(h,i)/3;local o,p=a(k,0,h/2,i/2)local d,e=0,0;for q=1,100 do d,e=a(k,math.pi*2*q/100,h/2,i/2)surface.DrawLine(o,p,d,e)o,p=d,e end end else local k=math.min(h,i)/3;local o,p=a(k,0,h/2,i/2)local d,e=0,0;for q=1,100 do d,e=a(k,math.pi*2*q/100,h/2,i/2)surface.DrawLine(o,p,d,e)o,p=d,e end end end;vgui.Register("DCircleVisualizer",visualizer,"DVisualizer")
